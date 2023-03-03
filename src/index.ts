8755b6fc3aff9f0b27a01ad997ce9b81bc4ae41b
Alvin Leung <alvinleung2009@gmail.com>
2023-03-03 10:09:30 -0800

    ask question using chat gpt api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { ChatGPTAPI, ChatGPTUnofficialProxyAPI } from "chatgpt";
 import express, { Request, Response } from "express";
 import inquirer from "inquirer";
+import * as dotenv from "dotenv";
+import Keyv from "keyv";
+import { oraPromise } from "ora";
 
 // const app = express();
 // const port = 10000;
@@ -12,10 +16,27 @@ import inquirer from "inquirer";
 //   console.log(`🚀 server started at http://localhost:${port}`);
 // });
 
+//https://chat.openai.com/api/auth/session
+dotenv.config();
+
+// const api = new ChatGPTUnofficialProxyAPI({
+//   accessToken: process.env.OPENAI_ACCESS_TOKEN as string,
+// });
+
+const messageStore = new Keyv();
+const api = new ChatGPTAPI({
+  apiKey: process.env.OPENAI_API_KEY as string,
+  messageStore,
+});
+
 (async () => {
   while (true) {
-    const ans = await inquirer.prompt([
-      { type: "input", name: "ans", message: "hello?" },
+    const { prompt } = await inquirer.prompt([
+      { type: "input", name: "prompt", message: "ask gpt:" },
     ]);
+    const res = await oraPromise(api.sendMessage(prompt), {
+      text: prompt,
+    });
+    console.log("\n" + res.text + "\n");
   }
 })();
